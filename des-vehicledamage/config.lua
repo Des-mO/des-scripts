6b7fcfe32036a1f65ff0eea74a0428bd6335ec28
MrBunny11 <130010917+MrBunny11@users.noreply.github.com>
2023-06-08 11:33:40 -0500

    optimize
    smoof

--- des-vehicledamage/config.lua
+++ des-vehicledamage/config.lua
@@ -1,14 +1,12 @@
 Config = {}
 
--- Minimum collision velocity required to cause damage
-Config.MinimumCollisionVelocity = 5.0 -- Adjust this value as desired
+-- Collision damage
+Config.CollisionDamageMultiplier = 0.2 -- Multiplier for collision damage (adjust as needed)
+Config.MinimumCollisionVelocity = 5.0 -- Minimum collision velocity to cause damage (adjust as needed)
 
--- Collision damage multiplier based on velocity
-Config.CollisionDamageMultiplier = 0.2 -- Adjust this value as desired
-
--- Damage modifiers for different weapon types
--- Modify these values to adjust the damage multipliers for specific weapon types
+-- Weapon damage modifiers
 Config.WeaponDamageModifiers = {
-    [GetHashKey("WEAPON_PISTOL")] = 1.0, -- Pistol damage modifier
-    [GetHashKey("WEAPON_SMG")] = 1.5 -- SMG damage modifier
+    [GetHashKey("WEAPON_PISTOL")] = 0.5, -- Pistol damage modifier (adjust as needed)
+    [GetHashKey("WEAPON_SMG")] = 0.8, -- SMG damage modifier (adjust as needed)
+    [GetHashKey("WEAPON_RIFLE")] = 1.2 -- Rifle damage modifier (adjust as needed)
 }
--- des-vehicledamage/server/server.lua
+++ des-vehicledamage/server/server.lua
@@ -1,5 +1,3 @@
-local previousHealth = {}
-
 function CalculateCollisionDamage(velocity)
     local collisionDamageMultiplier = Config.CollisionDamageMultiplier
     return velocity * collisionDamageMultiplier
@@ -8,38 +6,40 @@ end
 function HandleVehicleDamage(attacker, weaponHash, damageAmount, damageType, entity)
     if IsEntityAVehicle(entity) then
         local vehicle = Entity(entity)
-        local currentHealth = vehicle:GetHealth()
+        local currentHealth = GetVehicleBodyHealth(vehicle)
 
-        if previousHealth[entity] then
-            if currentHealth < previousHealth[entity] then
-                if damageType == 1 then
-                    local collisionVelocity = #(previousHealth[entity] - currentHealth)
-                    if collisionVelocity > Config.MinimumCollisionVelocity then
-                        local collisionDamage = CalculateCollisionDamage(collisionVelocity)
-                        print("Vehicle collided with an object. Damage: " .. collisionDamage)
-                    end
-                end
-
-                local weaponDamageModifier = Config.WeaponDamageModifiers[weaponHash]
-                if weaponDamageModifier then
-                    local modifiedDamage = damageAmount * weaponDamageModifier
-                    print("Vehicle was shot by a player. Damage: " .. modifiedDamage)
+        if currentHealth < 1000 then
+            if damageType == 1 then
+                local collisionVelocity = (1000 - currentHealth) * 0.1
+                if collisionVelocity > Config.MinimumCollisionVelocity then
+                    local collisionDamage = CalculateCollisionDamage(collisionVelocity)
+                    print("Vehicle collided with an object. Damage: " .. collisionDamage)
                 end
             end
-        end
 
-        previousHealth[entity] = currentHealth
+            local weaponDamageModifier = Config.WeaponDamageModifiers[weaponHash]
+            if weaponDamageModifier then
+                local modifiedDamage = damageAmount * weaponDamageModifier
+                print("Vehicle was shot by a player. Damage: " .. modifiedDamage)
+            end
+        end
     end
 end
 
 RegisterNetEvent("vehicleDamage")
 AddEventHandler("vehicleDamage", function(attacker, weaponHash, damageAmount, damageType, entity)
-    local source = source
+    local src = source
+    local xPlayer = QBCore.Functions.GetPlayer(src)
+    local playerPed = xPlayer.PlayerData.ped
+    local vehicle = GetVehiclePedIsIn(playerPed, false)
+    local entity = vehicle or playerPed
+
     HandleVehicleDamage(attacker, weaponHash, damageAmount, damageType, entity)
 end)
 
 RegisterCommand("damageVehicle", function(source, args, rawCommand)
-    local playerPed = GetPlayerPed(source)
+    local xPlayer = QBCore.Functions.GetPlayer(source)
+    local playerPed = xPlayer.PlayerData.ped
     local attacker = playerPed
     local weaponHash = GetSelectedPedWeapon(playerPed)
     local damageAmount = tonumber(args[1])
